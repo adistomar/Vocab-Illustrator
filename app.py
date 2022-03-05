3842c8d3b7cf820b7c7b1d23b7194323ecc1a1c9
aditya2604 <aditya26042005@gmail.com>
2022-03-05 14:37:55 -0800

    added HTR and flask server

---
+++ app.py
@@ -0,0 +1,25 @@
+#!flask/bin/python
+from flask import Flask, jsonify
+from main import *
+import SimpleHTR
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+   return "Hello, World!"
+
+@app.route('/getRandomWord', methods=['GET'])
+def getRandomWord():
+    return jsonify(returnWord())
+
+@app.route('/getWordFromInput', methods=['GET'])
+def getWordFromInput(word):
+    return jsonify(getWord(word))
+
+@app.route('/getWordFromImg', methods=['GET'])
+def getWordFromImg(address):
+   return jsonify(getWord(SimpleHTR.src.main.main(address)))
+
+if __name__ == '__main__':
+   app.run(debug=True)
