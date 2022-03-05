ad8702ebe0a43bffb29e959af1a2e3aa061e911a
aditya2604 <aditya26042005@gmail.com>
2022-03-05 12:09:18 -0800

    returns word tuple of inputted word

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import csv
 import random
+from PyDictionary import PyDictionary
+import urllib.request
+from bs4 import BeautifulSoup
 
 words = []
 PoS = []
@@ -14,7 +17,7 @@ with open('words.csv') as csv_file:
       defs.append(row[3])
       examples.append(row[4])
 
-def getWord():
+def returnWord():
    global words
    global PoS
    global defs
@@ -24,4 +27,24 @@ def getWord():
    word_index = words.index(word)
    return (word, PoS[word_index], defs[word_index], examples[word_index])
 
-print(getWord())
+def getWord(word):
+   word = str(word)
+   dictionary = PyDictionary()
+
+   url = "https://www.vocabulary.com/dictionary/" + word + ""
+   htmlfile = urllib.request.urlopen(url)
+   soup = BeautifulSoup(htmlfile, 'lxml')
+
+   try:
+      definition = dictionary.meaning(word)
+      PoS = list(definition.keys())[0].lower()
+      definition = list(definition.items())[0][1][0]
+      try:
+         example = soup.find(class_="example").get_text().replace("\n", "")
+      except:
+         example = ""
+   except:
+      return 'Cannot find such word! Check spelling.'
+   
+   return (word, PoS, definition, example)
+
