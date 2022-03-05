a352031797143d7e037397b6a0db68aee176e639
aditya2604 <aditya26042005@gmail.com>
2022-03-05 10:09:10 -0800

    added getWord command

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import csv
+import random
 
 words = []
 PoS = []
@@ -13,7 +14,14 @@ with open('words.csv') as csv_file:
       defs.append(row[3])
       examples.append(row[4])
 
-print(words)
-print(PoS)
-print(defs)
-print(examples)
+def getWord():
+   global words
+   global PoS
+   global defs
+   global examples
+
+   word = str(random.choice(words))
+   word_index = words.index(word)
+   return (word, PoS[word_index], defs[word_index], examples[word_index])
+
+print(getWord())
